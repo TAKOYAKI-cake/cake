a1bf9d6523c4797701f185e1ef71538dddab0b65
yuuta66jp <yuuta66jp@gmail.com>
2020-02-08 07:29:43 +0000

    Add [orderモデル、admin用テーブル作成]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # admin用loginsessionsのため編集
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # adminログイン後topへ移動
+  root 'admin/orders#top'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_032912) do
+ActiveRecord::Schema.define(version: 2020_02_08_062825) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,36 @@ ActiveRecord::Schema.define(version: 2020_02_08_032912) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "total_amount"
+    t.integer "order_status"
+    t.string "method_of_payment"
+    t.string "postcode"
+    t.string "shipping_address"
+    t.string "shipping_name"
+    t.integer "postage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_id"
+    t.integer "sales_status"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
