ed2996f9756a361ac2a9eac3bd6e6419c126b529
Nishimurasyota <syota4723@gamil.com>
2020-02-08 06:30:25 +0000

    Add [controllerとview(管理者側の商品とジャンル)]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'products/index'
+    get 'products/show'
+    get 'products/new'
+    get 'products/create'
+    get 'products/edit'
+    get 'products/update'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
