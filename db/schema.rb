9d116b529a81c521a3ce6d30e7abec08be7c3a88
Nishimurasyota <syota4723@gamil.com>
2020-02-09 06:44:16 +0000

    Fix [product/model]

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_02_09_041959) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "post_code"
+    t.string "shipping_address"
+    t.string "shipping_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,6 +34,18 @@
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "creditcards", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "card_id"
+    t.string "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -55,6 +78,13 @@
     t.datetime "updated_at", null: false
   end
 
+  create_table "orderd_products", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "price"
+    t.integer "making_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
