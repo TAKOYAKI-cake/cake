8e7f0eea87d9261bc4b942e24387ab6789265127
yuuta66jp <yuuta66jp@gmail.com>
2020-02-09 03:26:17 +0000

    fix [admin画面、topページ]

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_052153) do
-
+ActiveRecord::Schema.define(version: 2020_02_08_084658) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +49,19 @@ ActiveRecord::Schema.define(version: 2020_02_08_052153) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "total_amount"
+    t.integer "order_status"
+    t.string "method_of_payment"
+    t.string "postcode"
+    t.string "shipping_address"
+    t.string "shipping_name"
+    t.integer "postage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
