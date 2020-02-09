2b354997052ef17f68e6f30a858d0a1bea4947a8
noareizu <noareizu417@gmail.com>
2020-02-09 12:18:52 +0000

    ログイン成功時遷移先振り分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-       admin_orders_top_path
+    	case resource
+    	when Admin
+        	admin_orders_top_path
+    	when Customer
+            root_path
+        end
     end
 
     protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  root to: 'customers#top'
 #customerのルーティング
 resources :addresses
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :products, only: [:show, :index, :edit, :update, :new, :create]
+  end
+
+  namespace :customer  do
+    resources :customers, only: [:show]
   end
 
   devise_for :admins
