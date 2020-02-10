f08931bb6d861e2c3c053c8be3fb3bd62cdbc771
noareizu <noareizu417@gmail.com>
2020-02-10 06:46:33 +0000

    Add[customer_edit画面作成]

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,42 @@
 class AddressesController < ApplicationController
+
+  def create
+  	address = Address.new(address_params)#真祈投稿用空
+  	address.customer_id = current_customer.id#user_idは、コメントしたユーザのID、currentでログインしているユーザー
+    address.save! #DBへ保存（保存できているか確認のため！つけてます）
+    redirect_to "/addresses"# 配送先一覧・登録へリダイレクト
+  end
+
   def index
-  end
-
-  def show
-  end
-
-  def new
+  	@address = Address.new #新規登録用
+  	@addresses = Address.all #一覧
+  	@address.customer_id = current_customer.id#誰のカスタマーidをとるのか定義
+  	@customer = current_customer#ヘッダー
+  	@customer = current_customer #ヘッダー
   end
 
   def edit
+  	@address = Address.find(params[:id])#編集更新するレコードのidを取得
+  	@customer = current_customer#ヘッダー
+  	@customer = current_customer #ヘッダー
   end
+
+  def update
+  	@address = Address.find(params[:id])#更新するレコードのidを取得
+  	@address.update(address_params)# ストロングパラメーター
+  	redirect_to addresses_path(@address.id)# 配送先一覧・登録へリダイレクト
+  end
+
+  def destroy
+  	@address = Address.find(params[:id])#削除するレコードのidを取得
+    @address.destroy#削除
+    redirect_to addresses_path# 配送先一覧・登録へリダイレクト
+  end
+
+private
+
+  def address_params#create,update用ストロングパラメーター
+  	params.require(:address).permit(:post_code, :shipping_address, :shipping_name)
+  end
+
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,18 +4,24 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = current_customer
+    @customer = current_customer#customerのidを取得
   end
 
   def edit
-
+    @customer = current_customer#customerのidを取得
+  end
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path
   end
 
   def confirm
+    @customer = current_customer
   end
 
   private
   def customer_params
-    params.require(:user).permit(:family_name,:last_name,:family_name_kana,:last_name_kana,:post_code,:post_code,:telephone_number)
+    params.require(:customer).permit(:family_name,:last_name,:family_name_kana,:last_name_kana,:post_code,:post_code,:telephone_number)
   end
 end
