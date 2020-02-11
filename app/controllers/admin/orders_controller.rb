43799bbbca4c24d3c5829c551d9c681ea522ce74
yuuta66jp <yuuta66jp@gmail.com>
2020-02-11 04:11:44 +0000

    add [customer編集]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,20 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
 
   def top
   end
 
   def index
+    @orders = Oder.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+  end
+
+private
+
+  def order_params
+      params.require(:order).permit(:)
 end
