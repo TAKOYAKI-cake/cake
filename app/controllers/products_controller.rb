5adc05f286582518e7a492e5ceb6b3f18fad550a
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-02-11 04:05:37 +0000

    商品一覧などとちゅう

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,26 @@
 class ProductsController < ApplicationController
-  def index
+  def index#一覧、ジャンル検索一覧
   	@customer = current_customer#ヘッダー
-#サイドジャンル検索ようのが必要
-    @products = Product.all#productテーブルから全て取り出す
-    @products = Product.page(params[:page]).reverse_order#kaminariページャ用のため
+    @genres = Genre.all#ジャンルサイドバー一覧
+
+    if params[:genre_id]# urlにgenre_id(params)がある場合
+        @genre = Genre.find(params[:genre_id])# Genreのデータベースのテーブルから一致するidを取得
+        @products = @genre.products.all# genre_idと紐づく投稿を取得
+        @count = @products.count#@productsは7行目、genre_idと紐づいたテーブル数
+        @title = genre.name #genre_id(params)がある時の一覧タイトル
+      else
+        @products = Product.all#productテーブルから全て取り出す
+        @count = Product.count#product内全件の数
+        @title = "商品一覧"#genre_id(params)がない時の一覧タイトル
+    end
+
+    @products = Product.page(params[:page]).reverse_order#kaminariページャ用のため、７、１１行目と同じ名前
   end
 
   def show#商品詳細,注文ボタン
+    @customer = current_customer#ヘッダー
+    @genres = Genre.all#ジャンルサイドバー一覧
+    @product = Product.find(params[:id])#商品のレコードのid取得
   end
 
   def new
