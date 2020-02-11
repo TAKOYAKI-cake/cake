9877d21b757836a1683f5fb2567ae18acff04763
Nishimurasyota <syota4723@gamil.com>
2020-02-11 03:26:47 +0000

    fix [admin/product/view.product/model.schema]

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :genres, optional: true
+  belongs_to :genre, optional: true
   has_many :cart_items
   enum status: { 販売中: 0, 販売停止中: 1 }
+
+  attachment :image
 
 end
