8c9e2d51127b89cbd8627fecc507ac5b2cfa5d57
kaori48 <kaori2501stargaingalaxy@gmail.com>
2020-02-13 02:58:17 +0000

    カート、ログイン制限

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,23 @@ class CartsController < ApplicationController
   	@cart_items =CartItem.all#cart_itemsテーブルから全て取り出す
   end
 
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_params)
+    redirect_to carts_path#カート内一覧
+  end
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy#データ（レコード）を削除
+    redirect_to carts_path#カート内一覧
+  end
+  def destroy_all#ルーティング追加予定
+    #本来はモデル.find().アソシエーションだけど
+    #ログインしてる人のidが欲しいので、deviceで自動定義してくれるcurrent_customerを使う、customerに紐づくカーとを消す処理
+    current_customer.cart_items.destroy_all
+    redirect_to carts_path#カート内一覧
+  end
+
   def show
   end
 
