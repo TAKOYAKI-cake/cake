3012486904325a00864b1cb8213ad1a83aa84204
Nishimurasyota <syota4723@gamil.com>
2020-02-15 02:04:19 +0000

    fix [orderd_product/controller]

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,21 +5,34 @@ class OrdersController < ApplicationController
     @customer = current_customer
     @order = Order.new
   end
-  
+
   def index
     @customer = current_customer
-    @orders = Order.all
+      @orders = Order.all
   end
 
   def create
     @order = current_customer.orders.new(order_params)
     @order.save
-    redirect_to orders_path
+    @carts = current_customer.cart_items
+    @carts.each do |c|
+      orderd_product = OrderdProduct.new
+      # orderd_productに必要なカラムを入れる
+      orderd_product.product_id = c.product_id
+      orderd_product.quantity = c.quantity
+      orderd_product.price = c.product.price
+      orderd_product.order_id = @order.id
+      orderd_product.save!
+      # binding.pry
+    end
+    @carts.destroy_all
+    redirect_to orders_thanks_path
   end
 
   def confirm_new
     @customer = current_customer
     @order = @customer.orders.new(order_params)
+    @carts = current_customer.cart_items
 
     if @order.shipping_address == ""
       @order.shipping_address = @customer.address
@@ -47,5 +60,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:user_id, :total_amount, :order_status, :method_of_payment, :postcode, :shipping_address, :shipping_name, :postage)
   end
+
+  # def orderd_product_params
+  #   params.require(:orderd_product).permit(:quantity, :price, :making_status, :order_id, :product_id)
+  # end
 
 end
