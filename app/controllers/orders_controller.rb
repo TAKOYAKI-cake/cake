4fab7431c6ccdab2b53a587d6fdb3fc924ce9a90
yuuta66jp <yuuta66jp@gmail.com>
2020-02-15 06:03:39 +0000

    add [注文情報入力]

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
 
-
   def new
     @customer = current_customer
     @order = Order.new
+    @address = Address.new
+    @order.orderd_products.build
   end
-  
+
   def index
     @customer = current_customer
     @orders = Order.all
@@ -14,38 +15,67 @@ class OrdersController < ApplicationController
   def create
     @order = current_customer.orders.new(order_params)
     @order.save
+    @address = current_customer.addresses.new
+    @address.shipping_address = @order.shipping_address
+    @address.post_code = @order.postcode
+    @address.shipping_name = @order.shipping_name
+
+    @address.save
+
+
     redirect_to orders_path
   end
 
   def confirm_new
+
     @customer = current_customer
     @order = @customer.orders.new(order_params)
+    @carts = CartItem.all
+    @address = params[:order][:address]
 
-    if @order.shipping_address == ""
-      @order.shipping_address = @customer.address
-    end
+    if params[:select_shipping_address] == "1"
+     @order.shipping_address = @customer.address
+     @order.postcode = @customer.post_code
+     @order.shipping_name = @customer.total_name
 
-    @address = Address.all
+   elsif
+     params[:select_shipping_address] == "2"
+     @order.shipping_address = params[:order][:shipping_address]
+     @select = Address.find_by(shipping_address: params[:order][:shipping_address])
+     @order.postcode = @select[:post_code]
+     @order.shipping_name = @select[:shipping_name]
+   else
+     params[:select_shipping_address] == "3"
+     @order.shipping_address = @address[:shipping_address]
+     @order.postcode = @address[:post_code]
+     @order.shipping_name = @address[:shipping_name]
 
-    render :new unless @order.valid?
+   end
 
-  end
+   render :new unless @order.valid?
 
-  def show
-    @customer = current_customer
-    @order = Order.find(params[:id])
-  end
+ end
 
-  def edit
-  end
+ def show
+  @customer = current_customer
+  @order = Order.find(params[:id])
+end
 
-  def thanks
-    @customer = current_customer
-  end
+def edit
+end
 
-  private
-  def order_params
-    params.require(:order).permit(:user_id, :total_amount, :order_status, :method_of_payment, :postcode, :shipping_address, :shipping_name, :postage)
-  end
+def thanks
+  @customer = current_customer
+end
+
+private
+
+def order_params
+  params.require(:order).permit(:user_id, :total_amount, :order_status, :method_of_payment, :postcode, :shipping_address, :shipping_name, :postage, orderd_products_attributes: [:price])
+end
+
+def address_params
+  params.require(:address).permit(:customer_id, :post_code, :shipping_address, :shipping_name)
+end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   has_many :orderd_products
+  accepts_nested_attributes_for :orderd_products
   belongs_to :customer
   enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
   enum method_of_payment: { クレジットカード: 'card', 銀行振込: 'bank' }
 end
-
