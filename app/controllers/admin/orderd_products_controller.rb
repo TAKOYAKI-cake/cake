25ecff1ce81348ce69729e8a0da402acec9b23df
Nishimurasyota <syota4723@gamil.com>
2020-02-17 04:24:35 +0000

    fix[注文履歴詳細]

--- app/controllers/admin/orderd_products_controller.rb
+++ app/controllers/admin/orderd_products_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrderdProductsController < ApplicationController
   def update
+  	@orderd_product = OrderdProduct.find(params[:id])
+  	@orderd_product.update(orderd_product_params)
+  	#binding.pry
+    redirect_to admin_order_path(@orderd_product.id)
+  end
+  private
+  def orderd_product_params
+  	params.require(:orderd_product).permit(:quantity, :price, :making_status, :order_id, :product_id)
   end
 end
